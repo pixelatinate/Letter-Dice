6d6bf09b20b3059c418a0febf14681f4e137e30a
Swasti Mishra <smishr11@vols.utk.edu>
2021-11-08 16:59:07 -0500

    Uploading Files
    Copied a bunch of files over from the inital project repo.

---
+++ worddice.cpp
@@ -0,0 +1,24 @@
+// Project 05: Letter Dice
+// You can read about this challenge here: https://web.eecs.utk.edu/~semrich/cs302-21/assignments/proj05.html 
+// This lab takes input from imaginary dice with letters on each side and checks if 
+//      you can form certain words with the dice provided
+// COSC 302
+// Katie Nuchols and Swasti Mishra
+
+# include <map>
+# include <list>
+# include <vector>
+# include <fstream>
+# include <iostream>
+
+using namespace std ;
+
+enum customNodes{   wordNode ,
+                    sinkNode ,
+                    diceNode ,
+                    sourceNode , 
+                } ;  
+
+int main( int argc , char* argv[] ){
+    return 0 ;
+}
