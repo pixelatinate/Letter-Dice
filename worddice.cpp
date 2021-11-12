1712dc38f149da0b2f99384e063d153c555ee154
Katie L Nuchols <knuchol1@tesla1.eecs.utk.edu>
2021-11-12 15:19:50 -0500

    worddice.cpp changes
    i set up the initial stuff that we will need. some of the structs still need to be put together though the read in for the files is there and all the information seems to be correct within those.

--- worddice.cpp
+++ worddice.cpp
@@ -19,6 +19,51 @@ enum customNodes{   wordNode ,
                     sourceNode , 
                 } ;  
 
+struct Node{
+	nodeType type;//type of node being used
+	int id;//id of the variable
+	int visited;//if the variable has been visited themn this will change
+	vector<bool> letters;//all of the letters if used will change bool value
+	vector<class Edge*> adj;//use a vector of the Edge struct 
+	class Edge* backEdge;
+};
+
+struct Edge{
+
+};
+
+struct Graph{
+
+};
+
 int main( int argc , char* argv[] ){
+    string temp;
+	ifstream inFile;
+	vector<string> words;
+	vector<Node*> dice;
+	vector<Node*> currentLetters;
+	int i, j, k, l, idIterator;
+	Graph wordGraph;
+    //soime variables that will be used later
+    idIterator = 1;
+	inFile.open(argv[1]);//open the first file
+	while(getline(inFile, temp)){
+		Node * die = new Node;
+		die->type = DICE;
+		die->id = idIterator;
+		die->letters.resize(26);
+		for(j = 0; j < temp.length(); j++){
+			die->letters.at(temp.at(j)-65) = 1;
+		}
+		dice.push_back(die);
+		idIterator++;
+	}//get the information and add it to the node to be used later
+
+	inFile.close();//close the file once done
+	inFile.open(argv[2]);//open the next file
+	while(getline(inFile, temp)){
+		words.push_back(temp);//push back the information from the next file to be used later
+	}
+    
     return 0 ;
 }
