ee1c89e2964a87b69c2f77f81193d6aeb352147f
Swasti Mishra <smishr11@vols.utk.edu>
2021-11-12 23:37:19 -0500

    Updated main function and graph class
    made a couple changes to the commenting style and organization no biggie

--- worddice.cpp
+++ worddice.cpp
@@ -20,12 +20,12 @@ enum customNodes{   wordNode ,
                 } ;  
 
 struct Node{
-	nodeType type;//type of node being used
-	int id;//id of the variable
-	int visited;//if the variable has been visited themn this will change
-	vector<bool> letters;//all of the letters if used will change bool value
-	vector<class Edge*> adj;//use a vector of the Edge struct 
-	class Edge* backEdge;
+	customNodes type ;		// Type of node being used
+	int id ;				// ID of the variable
+	int visited ;			// If the variable has been visited then this will change
+	vector<bool> letters ;	// All of the letters if used will change bool value
+	vector<class Edge*> adj;// Use a vector of the Edge struct 
+	class Edge* backEdge ;
 };
 
 struct Edge{
@@ -33,37 +33,77 @@ struct Edge{
 };
 
 struct Graph{
+	bool BFS() ;
+	void clear() ;
+	bool confirm() ;
 
+	int minimum ;
+	vector<Node*> nodes ;
+	vector<int> usedDie ;	
 };
 
 int main( int argc , char* argv[] ){
-    string temp;
-	ifstream inFile;
-	vector<string> words;
-	vector<Node*> dice;
-	vector<Node*> currentLetters;
-	int i, j, k, l, idIterator;
-	Graph wordGraph;
-    //soime variables that will be used later
-    idIterator = 1;
-	inFile.open(argv[1]);//open the first file
-	while(getline(inFile, temp)){
-		Node * die = new Node;
-		die->type = DICE;
-		die->id = idIterator;
-		die->letters.resize(26);
-		for(j = 0; j < temp.length(); j++){
-			die->letters.at(temp.at(j)-65) = 1;
-		}
-		dice.push_back(die);
-		idIterator++;
-	}//get the information and add it to the node to be used later
+    
+	// Variables
+	string temp ;
+	ifstream inputFile ;
+	vector <Node*> dice ;
+	vector <Node*> letters ;
+	vector <string> words ;
+	int counter ;
+	Graph graph;
 
-	inFile.close();//close the file once done
-	inFile.open(argv[2]);//open the next file
-	while(getline(inFile, temp)){
-		words.push_back(temp);//push back the information from the next file to be used later
+	// Opens the file and pushes back the information
+    counter = 1 ;
+	inputFile.open( argv[1] ) ;
+	while( getline( inputFile, temp ) ){
+		Node * die = new Node ;
+		die->type = diceNode ;
+		die->id = counter ;
+		die->letters.resize( 26 ) ;
+		for( int j = 0 ; j < temp.length() ; j++ ){
+			die->letters.at( temp.at(j) - 'A' ) = 1 ;
+		}
+		dice.push_back( die ) ;
+		counter++ ;
+	}
+
+	// Closes the first file and opens the next 
+	inputFile.close() ;
+	inputFile.open( argv[2] ) ;
+
+	// Pushes back the words
+	while( getline( inputFile, temp ) ){
+		words.push_back( temp ) ;
 	}
     
+	for( int i = 0; i < words.size(); i++){
+		
+		// Builds the sink
+		Node* sink = new Node ;
+		sink->type = sinkNode ;
+		sink->id = ( words.at( i ).length() + dice.size() ) ;
+
+		// Builds the source
+		Node* source = new Node ;
+		source->type = sourceNode ;
+
+		// Pushes each letter back as its own node
+		for( int j = 0 ; j < words.at( i ).length() ; j++ ){
+			
+			// Creates the new node
+			Node letter* = new Node ;
+			
+			// Pushes the appropriate value back into the node
+			letter->id = counter ;
+			letter->type = wordNode ;
+			letter->letters.resize(26) ;
+			letter->letters.at( words.at(i).at(j) - 'A' ) = 1 ;
+
+			// Pushes each letter back
+			currentLetters.push_back( letter ) ;
+		}
+	}
+
     return 0 ;
 }
